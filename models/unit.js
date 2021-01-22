fb3d9536f3642bf937faecd33cb501170c525c98
tkadima <kadima.t@husky.neu.edu>
2021-01-22 16:04:13 -0500

    added more stuff

--- models/unit.js
+++ models/unit.js
@@ -1,9 +1,8 @@
 'use strict';
+const bcrypt = require('bcrypt');
+const { INTEGER } = require('sequelize/types');
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+unit.init({
+    id: integer,
+    favUnitId: integer
+})
