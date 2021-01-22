4e881e8f34a1f2d17509a49242a38c94c4470002
tkadima <kadima.t@husky.neu.edu>
2021-01-22 16:36:28 -0500

    hope Im adding  right stuff to the models

--- models/unit.js
+++ models/unit.js
@@ -1,7 +1,21 @@
 'use strict';
-const bcrypt = require('bcrypt');
-const { INTEGER } = require('sequelize/types');
+const bcrypt = require('bcrypt')
 
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+}
 unit.init({
     id: integer,
     favUnitId: integer
